dc0af2d931905ecc73d4f22306983cc6631429c9
jamnjeri <jamnjeri@gmail.com>
2024-07-28 14:18:41 +0300

    Foreign key syntax changed for order details table

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -48,11 +48,12 @@ mycursor.execute("""
 mycursor.execute("""
   CREATE TABLE IF NOT EXISTS Orders_Details (
     order_detail INT AUTO_INCREMENT PRIMARY KEY,
-    order_id FOREIGN KEY REFERENCES Orders(order_id),
-    book_id FOREIGN KEY REFERENCES Books(book_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
   )
 """)
 
+mydb.commit()
 mycursor.close()
 mydb.close()
