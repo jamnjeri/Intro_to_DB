dc6368e0df6ff21bf288d6cd397ccd97e4ab815a
jamnjeri <jamnjeri@gmail.com>
2024-07-28 14:26:29 +0300

    Orders_Details changed to Order_Details

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -47,7 +47,7 @@ mycursor.execute("""
 """)
 
 mycursor.execute("""
-  CREATE TABLE IF NOT EXISTS Orders_Details (
+  CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
